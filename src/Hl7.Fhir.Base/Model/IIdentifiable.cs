1a8a02c0a2af6249d0a02a547aee206b7bb82fc2
Waeber Philippe (Galenica) <philippe.waeber@galenica.com>
2023-05-25 18:07:19 +0200

    Create IIdentifiable interface and create two partial classes to apply them on

---
+++ src/Hl7.Fhir.Base/Model/IIdentifiable.cs
@@ -0,0 +1,13 @@
+﻿using Hl7.Fhir.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hl7.Fhir.Base.Model;
+
+public interface IIdentifiable
+{
+    List<Identifier> Identifier { get; set; }
+}
